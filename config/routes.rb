1fc3525ac0662f9c75b684a4ae2b0228747b5f34
Sebastian <saguerrero@uc.cl>
2018-04-02 01:24:40 -0300

    commitcommit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :talks
   end
   devise_for :users
-  #root 'notes#index'
+  root 'notes#index'
   namespace :api do
     namespace :v1 do
       resources :notes, path: "entries" do
